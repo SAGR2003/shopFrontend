24cf7fa01600d4543d77a8f6df1c148076d57b93
Sergio Gonzalez <sagr20032006@gmail.com>
2023-05-20 20:26:51 -0500

    update listProductByCode

--- src/ShopApp.jsx
+++ src/ShopApp.jsx
@@ -29,7 +29,7 @@ export const ShopApp = () => {
         if (product) {
             setProducts([product]);
         } else {
-            setProducts([]);
+            loadProducts();
         }
     };
 
